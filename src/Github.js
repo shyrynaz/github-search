2bbe720e177c133b70a24d2690f4a46c3e4716a2
shyrynaz <muchirimukami822@gmail.com>
2018-12-10 13:34:48 +0300

    initial commit

---
+++ src/Github.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Profile from './Components/Profile';
+import Search from './Components/Search';
+
+
+const API = 'https://api.github.com/users';
+class Github extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state ={
+            username: 'shyrynaz',
+            name: '',
+            avatar: '',
+            repos: '',
+            followers: '',
+            following: '',
+            homeURL: '',
+            notFound: ''
+        };
+    }
+
+    getProfile(username){
+        let finalURL = `${API}/${username}`;
+
+        fetch(finalURL)
+        .then((res) => res.json() )
+        .then((data) => {
+            this.setState({
+                username: data.login,
+                name: data.name,
+                avatar: data.avatar_url,
+                repos: data.public_repos,
+                followers: data.followers,
+                following: data.following,
+                homeURL: data.home_url,
+                notFound: data.message
+            })
+        })
+        .catch((error) => console.log('Problem fetching data'));
+    }
+
+    componentDidMount(){
+        this.getProfile(this.state.username);
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <section id="card">
+                  <Search searchProfile={this.getProfile.bind(this)}/>
+                  <Profile userData={this.state} />
+                </section>
+            </div>
+         );
+    }
+}
+ 
+export default Github;
