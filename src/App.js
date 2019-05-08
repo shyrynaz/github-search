97756184a48bb125752729cdbf6d5f8a402b0f1f
shyrynaz <muchirimukami822@gmail.com>
2019-05-08 16:23:15 +0300

    ADD login button

--- src/App.js
+++ src/App.js
@@ -1,103 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Github from './Github';
-import Header from './Components/Header';
-import Auth0Lock from 'auth0-lock';
-
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Github from "./Github";
+import Header from "./Components/Header";
+import Auth0Lock from "auth0-lock";
 
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      accessToken: '',
-      profile: {
-
-      }
-    }
+      accessToken: "",
+      profile: {}
+    };
   }
 
   static defaultProps = {
-    clientID: 'zuW50e3CBMqdV7NYP8oheyWuKbTCa6b2',
-    domain: 'shiri.auth0.com'
-  }
+    clientID: "zuW50e3CBMqdV7NYP8oheyWuKbTCa6b2",
+    domain: "shiri.auth0.com"
+  };
 
-  componentWillMount(){
+  componentWillMount() {
     this.lock = new Auth0Lock(this.props.clientID, this.props.domain);
 
-    this.lock.on('authenticated', (authResult) => {
+    this.lock.on("authenticated", authResult => {
       //console.log(authResult);
 
-    this.lock.getProfile(authResult.accessToken, (error, profile) =>{
-      if(error){
-        console.log(error);
-        return;
-      }
+      this.lock.getProfile(authResult.accessToken, (error, profile) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
 
-      //console.log(profile);
-      this.setProfile(authResult.accessToken, profile);
-    });
-
+        //console.log(profile);
+        this.setProfile(authResult.accessToken, profile);
+      });
     });
 
     this.getProfile();
   }
 
-  setProfile(accessToken, profile){
-    localStorage.setItem('accessToken', accessToken);
-    localStorage.setItem('profile', JSON.stringify(profile));
+  setProfile(accessToken, profile) {
+    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("profile", JSON.stringify(profile));
 
     this.setState({
-      accessToken: localStorage.getItem('accessToken'),
-      profile: JSON.parse(localStorage.getItem('profile'))
+      accessToken: localStorage.getItem("accessToken"),
+      profile: JSON.parse(localStorage.getItem("profile"))
     });
   }
 
-  getProfile(){
-    if(localStorage.getItem('accessToken') != null){
-      this.setState({
-        accessToken: localStorage.getItem('accessToken'),
-        profile: JSON.parse(localStorage.getItem('profile'))
-      }, () => {
-        console.log(this.state);
-      });
+  getProfile() {
+    if (localStorage.getItem("accessToken") != null) {
+      this.setState(
+        {
+          accessToken: localStorage.getItem("accessToken"),
+          profile: JSON.parse(localStorage.getItem("profile"))
+        },
+        () => {
+          console.log(this.state);
+        }
+      );
     }
   }
 
-  showLock(){
+  showLock() {
     this.lock.show();
   }
-  logout(){
-       this.setState({
-         accessToken: '',
-         profile: ''
-       }, () => {
-         localStorage.removeItem('accessToken');
-         localStorage.removeItem('profile');
-       }); 
+  logout() {
+    this.setState(
+      {
+        accessToken: "",
+        profile: ""
+      },
+      () => {
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("profile");
+      }
+    );
   }
 
   render() {
     let gitter;
 
-    if(this.state.accessToken){
-      gitter =  <Github/>
-    } else{
-      gitter = "Click on login to view github"
+    if (this.state.accessToken) {
+      gitter = <Github />;
+    } else {
+      gitter = <button className="primary">Login</button>;
     }
     return (
       <div className="App">
-      <Header
-        lock={this.lock}
-        accessToken={this.state.accessToken}
-        onLogout={this.logout.bind(this)}
-        onLogin = {this.showLock.bind(this)}
-      />
-       {gitter}
+        <Header
+          lock={this.lock}
+          accessToken={this.state.accessToken}
+          onLogout={this.logout.bind(this)}
+          onLogin={this.showLock.bind(this)}
+        />
+        {gitter}
       </div>
     );
   }
